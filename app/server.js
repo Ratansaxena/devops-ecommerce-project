ef86a3821f0335781d64ac6e9d3e7b06b2054886
Ratansaxena <ratanverma31@gmail.com>
2026-06-23 16:47:01 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps E-Commerce Project Running Successfully version 2🚀");
+  res.send("DevOps E-Commerce Project Running Successfully version 2 made by ratan 🚀");
 });
 
 app.listen(3000, () => {
